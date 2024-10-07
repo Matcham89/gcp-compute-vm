06858a50b1003f7252cb55c52e02370e5c7186f2
Matcham89 <cjmatcham@hotmail.co.uk>
2024-10-07 08:44:35 +0100

    inital commit

---
+++ startup-script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Function to handle errors
+handle_error() {
+    echo "Error on line $1"
+    exit 1
+}
+
+# Trap errors and call handle_error
+trap 'handle_error $LINENO' ERR
+
+# Connect to Google Cloud
+echo "Connecting to Google Cloud"
+gcloud init || { echo "Failed to initialize gcloud"; exit 1; }
+
+# Create a Google project
+echo "Enter Project Name"
+read project_id
+
+echo "Creating project '${project_id}'"
+gcloud projects create $project_id || { echo "Failed to create project"; exit 1; }
+gcloud config set project $project_id || { echo "Failed to set project"; exit 1; }
+
+# Enable compute services API
+gcloud services enable compute.googleapis.com || { echo "Failed to enable compute services"; exit 1; }
+
+# Enter user email address
+echo "Enter user email address"
+read email
+
+# Set user permissions
+gcloud projects add-iam-policy-binding $project_id --member="user:${email}" --role=roles/compute.instanceAdmin.v1 || { echo "Failed to add IAM policy binding"; exit 1; }
+
+echo "Google Cloud settings configured"
+echo "Applying Terraform!"
+
+terraform init || { echo "Terraform init failed"; exit 1; }
+terraform plan || { echo "Terraform plan failed"; exit 1; }
+terraform apply || { echo "Terraform apply failed"; exit 1; }
+
+echo "Connecting to new VM"
+gcloud compute ssh --zone=us-central1-a chris-matcham-demo || { echo "Failed to connect to VM"; exit 1; }
